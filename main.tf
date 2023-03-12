dad372a31512f563349e4aaed2835dde3ca6b741
Smiddie31 <smiddie31@gmail.com>
2023-03-12 20:57:55 +1100

    Corrected cidr block

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_route_table" "publicrt" {
   vpc_id = aws_vpc.vpc.id
 
   route {
-    cidr_block = "0.0.0.0"
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 }
